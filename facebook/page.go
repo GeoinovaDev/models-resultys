7c83d0eb80ed4eb02d51c89515bd4e224249b8bf
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-04-23 15:55:27 -0300

    sync

--- facebook/page.go
+++ facebook/page.go
@@ -2,16 +2,16 @@ package facebook
 
 // Page struct
 type Page struct {
-	Nome     string
-	Title    string
-	URL      string
-	Endereco string
-	Curtidas string
-	Horarios string
+	Nome     string `json:"nome"`
+	Title    string `json:"title"`
+	URL      string `json:"url"`
+	Endereco string `json:"endereco"`
+	Curtidas string `json:"curtidas"`
+	Horarios string `json:"horarios"`
 
-	Telefones []string
-	Sites     []string
-	Emails    []string
+	Telefones []string `json:"telefones"`
+	Sites     []string `json:"sites"`
+	Emails    []string `json:"emails"`
 }
 
 // PopuleFromMap page
@@ -19,6 +19,30 @@ func (page *Page) PopuleFromMap(m map[string]interface{}) {
 	telefones := m["telefones"].([]interface{})
 	emails := m["emails"].([]interface{})
 	sites := m["sites"].([]interface{})
+
+	if val, ok := m["nome"]; ok {
+		page.Nome = val.(string)
+	}
+
+	if val, ok := m["title"]; ok {
+		page.Title = val.(string)
+	}
+
+	if val, ok := m["url"]; ok {
+		page.URL = val.(string)
+	}
+
+	if val, ok := m["endereco"]; ok {
+		page.Endereco = val.(string)
+	}
+
+	if val, ok := m["curtidas"]; ok {
+		page.Curtidas = val.(string)
+	}
+
+	if val, ok := m["horarios"]; ok {
+		page.Horarios = val.(string)
+	}
 
 	for i := 0; i < len(telefones); i++ {
 		page.Telefones = append(page.Telefones, telefones[i].(string))
