001794d77cee547b6a3c69e14b2d3ab8e9ed82ba
manoelnecojr@gmail <necoengenheiro@gmail.com>
2018-05-17 22:58:16 -0300

    add classify motor models

---
+++ pabx/classify/classify.go
@@ -0,0 +1,43 @@
+package classify
+
+import (
+	"models/empresa"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Classify struct
+type Classify struct {
+	ClassifyID bson.ObjectId  `json:"classify_id" bson:"_id,omitempty"`
+	ID         string         `bson:"id"`
+	Empresa    *empresa.Basic `json:"empresa" bson:"empresa"`
+
+	Pertence    []string `json:"pertence" bson:"pertence"`
+	NaoPertence []string `json:"nao_pertence" bson:"nao_pertence"`
+	Contador    []string `json:"contador" bson:"contador"`
+	Valido      []string `json:"valido" bson:"valido"`
+	NaoValidado []string `json:"nao_validado" bson:"nao_validado"`
+}
+
+// New ...
+func New() Classify {
+	return Classify{}
+}
+
+// ExistTelefoneValidado ...
+func (classify *Classify) ExistTelefoneValidado() bool {
+	if len(classify.Pertence) > 0 {
+		return true
+	}
+
+	return false
+}
+
+// ExistTelefoneValido ...
+func (classify *Classify) ExistTelefoneValido() bool {
+	if len(classify.Valido) > 0 {
+		return true
+	}
+
+	return false
+}
