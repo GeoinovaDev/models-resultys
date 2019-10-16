73e9c57c9b26e89766a0200050ccc1538defe51f
manoelnecojr@gmail <necoengenheiro@gmail.com>
2019-10-16 14:37:41 -0300

    extrai somente 2 primeiras palavras da razao social

--- empresa/basic.go
+++ empresa/basic.go
@@ -14,11 +14,13 @@ func (basic *Basic) GetNome() string {
 	nome := ""
 
 	if len(basic.Fantasia) == 0 {
-		partes := strings.Split(basic.RazaoSocial, " ")
-		if len(partes) >= 2 {
-			nome = partes[0] + " " + partes[1]
-		} else {
-			nome = partes[0]
+		if len(basic.RazaoSocial) > 0 {
+			partes := strings.Split(basic.RazaoSocial, " ")
+			if len(partes) >= 2 {
+				nome = partes[0] + " " + partes[1]
+			} else {
+				nome = partes[0]
+			}
 		}
 	} else {
 		nome = basic.Fantasia
