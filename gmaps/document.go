64f10dfb48739166e0c137ae456b1e45179bda94
manoelnecojr@gmail <necoengenheiro@gmail.com>
2020-05-31 14:07:15 -0300

    add nota do comentario

--- gmaps/document.go
+++ gmaps/document.go
@@ -8,11 +8,11 @@ type Comment struct {
 
 // Company ...
 type Company struct {
-	Name     string    `json:"name"`
-	LatLng   []string  `json:"latlng"`
-	Address  string    `json:"address"`
-	Phones   []string  `json:"phone"`
-	Sites    []string  `json:"site"`
-	Comments []Comment `json:"comments"`
-	Status   string    `json:"status"`
+	Name    string   `json:"name"`
+	LatLng  []string `json:"latlng"`
+	Address string   `json:"address"`
+	Phones  []string `json:"phone"`
+	Sites   []string `json:"site"`
+	Status  string   `json:"status"`
+	Nota    float32  `json:"nota"`
 }
